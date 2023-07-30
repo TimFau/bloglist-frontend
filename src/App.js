5fd8d99a3185292a16d7bd74bf824378235dd75b
Tim Fau <dev@timfau.com>
2023-07-30 10:48:35 -0400

    Exercises 5.3-5.4

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import './css/main.css'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessages, setErrorMessages] = useState([])
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [createdTitle, setCreatedTitle] = useState('')
+  const [createdAuthor, setCreatedAuthor] = useState('')
+  const [createdUrl, setCreatedUrl] = useState('')
 
   useEffect(() => {
     let lsUser = localStorage.getItem('user')
     if (user) {
-      blogService.getAll().then(blogs =>
-        setBlogs( blogs )
-      )  
+      getBlogs()
     }
     if(lsUser && !user) {
       const user = JSON.parse(lsUser)
@@ -23,38 +26,95 @@ const App = () => {
     }
   }, [user])
 
+  const getBlogs = () => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    ) 
+  }
+
   const handleLogin = async (event) => {
-    console.log('handleLogin')
     event.preventDefault()
 
     try {
       const user = await loginService.login({username, password})
       setUser(user)
-      console.log(user)
+      // console.log(user)
       setUsername('')
       setPassword('')
       localStorage.setItem('user', JSON.stringify(user))
+      handleSetSuccessMessage('Logged in')
     } catch (exception) {
-      setErrorMessage('Username or Password is incorrect')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+      handleSetErrorMessages(['Username or Password is incorrect'])
 
     }
   }
 
   const handleLogout = () => {
     setUser(null)
+    handleSetSuccessMessage('Logged out')
     localStorage.removeItem('user')
+  }
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const newBlog = await blogService.create(
+        { 
+          title: createdTitle,
+          author: createdAuthor,
+          url: createdUrl
+        },
+        user.token
+      )
+
+      const response = newBlog.response
+      if (newBlog.response && newBlog.response.data.errors) {
+        const errors = newBlog.response.data.errors
+        const errorMessages = Object.values(errors).map(error => error.message)
+        handleSetErrorMessages(errorMessages)
+      } else {
+        handleSetSuccessMessage(`Blog "${newBlog.title}" by ${newBlog.author} added`)
+        getBlogs()
+      }
+    } catch (exception) {
+      handleSetErrorMessages(['Error creating blog.'])
+    }
+  }
+
+  const handleResetMessages = () => {
+    setErrorMessages([])
+    setSuccessMessage(null)
+  }
+
+  const handleSetErrorMessages = (messages) => {
+    handleResetMessages()
+    setErrorMessages(messages)
+    setTimeout(() => {
+      handleResetMessages()
+    }, 5000)
+  }
+
+  const handleSetSuccessMessage = (message) => {
+    handleResetMessages()
+    setSuccessMessage(message)
+    setTimeout(() => {
+      handleResetMessages()
+    }, 5000)
   }
 
   if (user == null) {
     return (
       <div>
         <h2>Log in</h2>
-        <form onSubmit={handleLogin}>
+        {errorMessages.map(errorMessage => (
           <p className="error">{errorMessage}</p>
-          <div className="input-wrapper">
+        ))}
+        {successMessage && 
+          <p className="success">{successMessage}</p>
+        }
+        <form onSubmit={handleLogin}>
+          <div className="input-wrap">
             <label htmlFor="username">Username</label>
             <input
               id="username"
@@ -64,7 +124,7 @@ const App = () => {
               onChange={({ target }) => setUsername(target.value)}
             ></input>
           </div>
-          <div className="input-wrapper">
+          <div className="input-wrap">
             <label htmlFor="password">Password</label>
             <input
               id="password"
@@ -83,7 +143,48 @@ const App = () => {
   return (
     <div>
         <h2>Blogs</h2>
+        {errorMessages.map(errorMessage => (
+          <p className="error">{errorMessage}</p>
+        ))}
+        {successMessage && 
+          <p className="success">{successMessage}</p>
+        }
         <p>{user.name} logged in</p> <button type="button" onClick={handleLogout}>Logout</button>
+        <h3>Create New blog</h3>
+        <form onSubmit={handleCreateBlog}>
+          <div className="input-wrap">
+            <label htmlFor="title">Title</label>
+            <input
+              id="title"
+              type="text"
+              value={createdTitle}
+              name="Title"
+              onChange={({ target }) => setCreatedTitle(target.value)}
+            ></input>
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="author">Author</label>
+            <input
+              id="author"
+              type="text"
+              value={createdAuthor}
+              name="Author"
+              onChange={({ target }) => setCreatedAuthor(target.value)}
+            ></input>
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="url">URL</label>
+            <input
+              id="url"
+              type="text"
+              value={createdUrl}
+              name="URL"
+              onChange={({ target }) => setCreatedUrl(target.value)}
+            ></input>
+          </div>
+          <button type="submit">Create</button>
+        </form>
+
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -6,5 +6,19 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
+const create = async (params, token) => {
+  console.log('create blog', params, token)
+  const config = {
+    headers: { Authorization: `bearer ${token}` }
+  }
+  try {
+    const response = await axios.post(baseUrl, params, config)
+    return response.data
+  } catch (exception) {
+    console.log('exception', exception)
+    return exception
+  }
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+export default { getAll, create }
