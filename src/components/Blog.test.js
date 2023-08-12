abc69765835e19ad4626cf21f5ba7a54e00538ff
Tim Fau <dev@timfau.com>
2023-08-11 23:31:23 -0400

    Exercise 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    author: 'Test Author',
-    id: '123456789',
-    likes: 7,
-    title: 'Test Blog Post',
-    url: 'https://google.com',
-    user: {
-      id: '1234',
-      name: 'Test User',
-      username: 'raged'
-    }
+// Test Data
+const blog = {
+  author: 'Test Author',
+  id: '123456789',
+  likes: 7,
+  title: 'Test Blog Post',
+  url: 'https://google.com',
+  user: {
+    id: '1234',
+    name: 'Test User',
+    username: 'raged'
   }
+}
+
+// Tests
+test('renders only title and author by default', () => {
 
   const { container } = render(
     <Blog
@@ -33,6 +37,28 @@ test('renders content', () => {
   expect(container).toHaveTextContent(blog.title)
   expect(container).not.toHaveTextContent(blog.url)
   expect(container).not.toHaveTextContent(blog.likes)
+})
 
-  screen.debug()
+test('renders url and likes after clicking "view more"', async () => {
+
+  const { container } = render(
+    <Blog
+      blog={blog}
+      usersUsername='Test User'
+      handleDeleteBlog={() => {}}
+      handleIncrementLikes={() => {}}
+    />
+  )
+  const user = userEvent.setup()
+  const button = screen.getByRole('button')
+
+
+  expect(container).toBeDefined()
+
+  await user.click(button)
+
+  expect(container).toHaveTextContent(blog.author)
+  expect(container).toHaveTextContent(blog.title)
+  expect(container).toHaveTextContent(blog.url)
+  expect(container).toHaveTextContent(blog.likes)
 })
